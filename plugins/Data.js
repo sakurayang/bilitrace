ca95b0bb52f1513a4fc53c6e6c902744193a4086
sakurayang <1304274443@qq.com>
2019-10-23 11:00:44 +0800

    Fix Bugs

--- plugins/Data.js
+++ plugins/Data.js
@@ -3,11 +3,12 @@ const schedule = require('node-schedule');
 
 const config = require("../config.json");
 
-const database_type = config.database.type.toUpperCase();
-const database_path = database_type == "CSV" ? config.database.csv.path : config.database.mysql;
+//const DATABASE_TYPE = config.database.type.toLowerCase();
+const DATABASE_TYPE = "csv";
+//const DATABASE_PATH = DATABASE_TYPE == "csv" ? config.database.csv.path : config.database.mysql;
+const DATABASE_PATH = "./data/";
 
-
-const tid = {
+const TID = {
     28: "原创音乐",
     29: "音乐现场",
     30: "VOCALOID·UTAU",
@@ -29,20 +30,11 @@ async function sleep(time) {
 }
 
 class CreateScheduleJob {
-    get job() {
-        return this.job;
-    }
-    constructor(type, id, time, day = 3) {
-        this.type = String(type) || "rank";
-        this.id = String(id);
-        this.time = String(time) || "*/5 * * * *";
-        this.day = day || 3;
-        this.spider = new Spider(this.type, this.id);
-        this.data = new Data(this.type, this.id);
-        this.job = schedule.scheduleJob(this.time, async () => {
-            let info = await this.spider.getInfo();
-            this.data.write(info);
-        });
+    constructor(type, id, time = "*/5 * * * *", day = 3) {
+        this.job_type = String(type) || "rank";
+        this.job_id = String(id);
+        this.job_time = String(time) || "*/5 * * * *";
+        this.job_day = day || 3;
         return this;
     }
     cancel() {
@@ -51,16 +43,15 @@ class CreateScheduleJob {
 }
 
 class CreateRankScheduleJob extends CreateScheduleJob {
-    constructor(id, time, day = 3) {
-        this.type = "rank";
-        this.id = String(id);
-        this.day = day || 3;
-        this.time = String(time) || `* */${this.day} * * *`;
-        this.spider = new RankSpider(this.id);
-        this.data = new RankData(this.id);
-        this.job = schedule.scheduleJob(this.time, async () => {
+    constructor(id, time = "* * */5 * *", day = 3) {
+        super("rank", id, time, day);
+        this.job_type = "rank";
+        this.job_id = id;
+        this.spider = new RankSpider(this.job_id);
+        this.rank_data = new RankData(this.job_id);
+        this.job = schedule.scheduleJob(this.job_time, async () => {
             let info = await this.spider.getInfo();
-            this.data.write(info);
+            this.rank_data.write(info);
         });
         return this;
     }
@@ -68,28 +59,23 @@ class CreateRankScheduleJob extends CreateScheduleJob {
 
 class CreateVideoScheduleJob extends CreateScheduleJob {
     constructor(id, time) {
-        this.type = "video";
-        this.id = String(id);
-        this.time = String(time) || `*/5 * * * *`;
-        this.spider = new VideoSpider(this.id);
-        this.data = new VideoData(this.id);
-        this.job = schedule.scheduleJob(this.time, async () => {
+        super("video", id, time);
+        this.job_id = id;
+        this.job_type = "video";
+        this.spider = new VideoSpider(this.job_id);
+        this.video_data = new VideoData(this.job_id);
+        this.job = schedule.scheduleJob(this.job_time, async () => {
             let info = await this.spider.getInfo();
-            this.data.write(info);
+            this.video_data.write(info);
         });
         return this;
     }
 }
 
-module.exports = {
-    CreateScheduleJob,
-    CreateRankScheduleJob,
-    CreateVideoScheduleJob
-}
 
 class Data {
     get name() {
-        return this.path;
+        return this.data_path;
     }
 
     /**
@@ -99,9 +85,9 @@ class Data {
      * @return {Object}
      */
     constructor(datatype, id, path) {
-        this.datatype = datatype;
-        this.id = Number(id);
-        this.path = path;
+        this.data_type = datatype;
+        this.data_id = Number(id);
+        this.data_path = path;
         return this;
     }
 
@@ -109,25 +95,25 @@ class Data {
      * 
      * @returns {Void}
      */
-    write(data) {
-        if (database_type == "csv") {
+    write(write_data) {
+        if (DATABASE_TYPE == "csv") {
             const fs = require('fs');
-            fs.writeFile(this.path, data, {
+            fs.writeFile(this.data_path, write_data, {
                 flag: "a+",
                 encoding: "utf8"
             }, err => console.log(err));
-        } else if (database_type == "mysql") {
+        } else if (DATABASE_TYPE == "mysql") {
             const mysql = require('node-mysql-promise');
-            const conn = mysql.createConnection(this.path);
-            let table = this.datatype == "rank" ?
-                `rank_${this.id}` :
-                `video_${this.id}`;
-            let table_value = this.datatype == "rank" ?
+            const conn = mysql.createConnection(this.data_path);
+            let table = this.data_type == "rank" ?
+                `rank_${this.data_id}` :
+                `video_${this.data_id}`;
+            let table_value = this.data_type == "rank" ?
                 "(aid bigint primary key,title text,view bigint,coin bigint,danma bigint,favorite bigint,reply bigint,share bigint,heart_like bigint,pubdate text,update_date text)" :
                 "(aid int primary key, title text, tid int, tname text, rank int,date text,update_date text)";
             conn.query(`CREATE TABLE ${table} VALUE ${table_value}`);
             conn.table(table)
-                .addAll(data)
+                .addAll(write_data)
                 .catch(err => {
                     throw err
                 }).finally(() => conn.close());
@@ -145,9 +131,9 @@ class Data {
         let start = Number(limit.split('-')[0]);
         let end = Number(limit.split('-')[1]);
         console.log(start, end);
-        if (database_type == "csv") {
+        if (DATABASE_TYPE == "csv") {
             const readCSV = require('csvtojson');
-            let l_path = this.path;
+            let l_path = this.data_path;
             let l_data = [];
             let read_data = await readCSV({
                 noheader: 0,
@@ -162,14 +148,14 @@ class Data {
                     break;
                 }
             }
-        } else if (database_type == "mysql") {
+        } else if (DATABASE_TYPE == "mysql") {
             const mysql = require('node-mysql-promise');
-            const conn = mysql.createConnection(this.path);
-            let table = this.datatype == "rank" ?
-                `rank_${this.id}` :
-                `video_${this.id}`;
-            let select_options = this.datatype == "rank" ? {} : {
-                aid: this.id
+            const conn = mysql.createConnection(this.data_path);
+            let table = this.data_type == "rank" ?
+                `rank_${this.data_id}` :
+                `video_${this.data_id}`;
+            let select_options = this.data_type == "rank" ? {} : {
+                aid: this.data_id
             };
             let read_data = await conn.table(table)
                 .limit(end - start + 1)
@@ -185,25 +171,24 @@ class Data {
 class RankData extends Data {
     constructor(id) {
         let Path = require("path");
-        super("rank", id, this.path);
-        this.id = Number(id);
-        this.datatype = "rank";
-        this.path = database_type == csv ?
-            Path.normalize(`${database_path}${this.datatype}_${tid[id]}.csv`) :
-            database_path;
+        let l_path = DATABASE_TYPE == "csv" ?
+            Path.normalize(`${DATABASE_PATH}/rank_${TID[id]}.csv`) :
+            DATABASE_PATH;
+        super("rank", id, l_path);
+        this.data_id = Number(id);
+        this.data_type = "rank";
+        this.data_path = l_path;
 
         return this;
     }
     write(data) {
-        let data = data in Object && data != null ? data : {};
-        if (database_type == "csv") {
+        let write_data = data in Object && data != null ? data : {};
+        if (DATABASE_TYPE == "csv") {
             let head = "aid,view,coin,danma,favorite,reply,share,heart_like,point\n";
-            fs.stat(this.path, err => {
-                if (err) fs.writeFile(this.path, head, {
+            fs.stat(this.data_path, err => {
+                if (err) fs.writeFile(this.data_path, head, {
                     encoding: "utf8"
-                }, err => {
-                    throw err
-                });
+                }, err => {});
             });
             super.write([from.substr(4, 2),
             rank_info.rank_offset,
@@ -211,9 +196,9 @@ class RankData extends Data {
             `"${rank_info.title}"`,
             rank_info.pubdate,
             `"${rank_info.tag.toString()}`
-            ]).bind(this);
-        } else if (database_type == "mysql") {
-            super.write(data).bind(this);
+            ]);
+        } else if (DATABASE_TYPE == "mysql") {
+            super.write(write_data);
         } else {
             throw new Error("Type not support");
         }
@@ -224,28 +209,29 @@ class RankData extends Data {
 class VideoData extends Data {
     constructor(id) {
         let Path = require("path");
-        super("video", id, this.path);
-        this.id = Number(id);
-        this.datatype = "video";
-        this.path = database_type == csv ?
-            Path.normalize(`${database_path}${this.datatype}_${tid[id]}.csv`) :
-            database_path;
+        let l_path = DATABASE_TYPE == "csv" ?
+            Path.normalize(`${DATABASE_PATH}/video_${id}.csv`) :
+            DATABASE_PATH;
+        super("video", id, l_path);
+        this.data_id = Number(id);
+        this.data_type = "video";
+        this.data_path = l_path;
         return this;
     }
     write(data) {
-        let data = data in Object && data != null ? data : [];
-        if (database_type == "csv") {
+        let write_data = data in Object && data != null ? data : [];
+        if (DATABASE_TYPE == "csv") {
             let head = "aid,view,coin,danma,favorite,reply,share,heart_like,point\n";
-            fs.stat(this.path, err => {
-                if (err) fs.writeFile(this.path, head, {
+            fs.stat(this.data_path, err => {
+                if (err) fs.writeFile(this.data_path, head, {
                     encoding: "utf8"
                 }, err => {
                     throw err
                 });
             });
-            super.write(data.toString() + "\n");
-        } else if (database_type == "mysql") {
-            super.write(data);
+            super.write(write_data.toString() + "\n");
+        } else if (DATABASE_TYPE == "mysql") {
+            super.write(write_data);
         } else {
             throw new Error("Type not support");
         }
@@ -253,34 +239,19 @@ class VideoData extends Data {
     }
 }
 
-module.exports = {
-    Data,
-    RankData,
-    VideoData
-}
-
-
 class Spider {
-    static video_baseurl = `https://api.bilibili.com/x/web-interface/view?aid=${this.id}`;
-    static rank_baseurl = `https://api.bilibili.com/x/web-interface/ranking/region?rid=${this.id}&day=${this.time.day}`;
+    //    static video_baseurl = `https://api.bilibili.com/x/web-interface/view?aid=${this.id}`;
+    //    static rank_baseurl = `https://api.bilibili.com/x/web-interface/ranking/region?rid=${this.id}&day=${this.time.day}`;
 
-    set url() {
-        let l_type = this.spider_type.toLowerCase();
-        this.url = l_type == "rank" ?
-            rank_baseurl :
-            l_type == "video" ?
-                video_baseurl :
-                false;
-        return this.url;
-    }
-
-    get url() {
-        return this.url;
-    }
-
-    constructor(type, id) {
+    constructor(type, id, day = 3) {
         this.spider_type = type.toLowerCase();
-        this.id = id;
+        this.spider_id = id;
+        this.spider_day = day;
+        this.spider_url = this.spider_type == "rank" ?
+            `https://api.bilibili.com/x/web-interface/ranking/region?rid=${this.spider_id}&day=${this.spider_day}` :
+            this.spider_type == "video" ?
+                `https://api.bilibili.com/x/web-interface/view?aid=${this.spider_id}` :
+                false;
         return this;
     }
 }
@@ -288,18 +259,19 @@ class Spider {
 class VideoSpider extends Spider {
     constructor(id) {
         super("video", id);
+        this.spider_url = `https://api.bilibili.com/x/web-interface/view?aid=${this.spider_id}`;
         return this;
     }
     /**
      * @returns {JSON}
      */
     async getInfo() {
-        let data = await request.get(this.url);
-        data = JSON.parse(data).data;
-        let stat = data.stat;
+        let raw_data = await request.get(this.spider_url);
+        raw_data = JSON.parse(raw_data).data;
+        let stat = raw_data.stat;
         return {
-            aid: data.aid,
-            title: data.title,
+            aid: raw_data.aid,
+            title: raw_data.title,
             view: stat.view,
             coin: stat.coin,
             danma: stat.danmaku,
@@ -307,7 +279,7 @@ class VideoSpider extends Spider {
             reply: stat.reply,
             share: stat.share,
             heart_like: stat.like,
-            pubdate: data.pubdate,
+            pubdate: raw_data.pubdate,
             update_date: Math.floor(Date.now() / 1000)
         }
     }
@@ -315,34 +287,38 @@ class VideoSpider extends Spider {
 
 class RankSpider extends Spider {
     constructor(id, ...time) {
-        super("rank", id);
         time = time.length == 0 ? [Date.now()] : time;
-        let from = new Date(time[0]).toISOString().substr(0, 10);
-        let to = time.length > 1 ?
+        let from = time ?
+            new Date(time[0]).toISOString().substr(0, 10) :
+            new Date(Date.now()).toISOString().substr(0, 10);
+        let to = time && time.length > 1 ?
             new Date(time[1]).toISOString().substr(0, 10) :
-            from.substr(0, 8) + String(new Date(from).getDate + 3);
-        this.time = {
+            from.substr(0, 8) + String(Number(new Date(from).getDate()) + 3).padStart(2, 0);
+        let day = Number(to.substr(8, 2)) - Number(from.substr(8, 2));
+        super("rank", id, day);
+        this.spider_time = {
             from: from,
             to: to,
-            day = Number(to.substr(8, 2)) - Number(from.substr(8, 2))
+            day: day
         };
         return this;
     }
     async getInfo() {
-        let data = await request.get(this.url);
-        data = JSON.parse(data).data;
+        let raw_data = await request.get(this.spider_url);
+        raw_data = JSON.parse(raw_data).data;
         let now_date = Date.now();
         let db_data = [];
-        for (let rank = 0, len = data.length; rank < len; rank++) {
-            let video = data[rank];
+        //console.log(data);
+        for (let rank = 0, len = raw_data.length; rank < len; rank++) {
+            let video = raw_data[rank];
             db_data.push({
                 aid: video.aid,
                 title: video.title,
-                tid: id,
+                tid: this.spider_id,
                 tname: video.typename,
                 author_name: video.author,
                 author_mid: video.mid,
-                rank: index + 1,
+                rank: rank + 1,
                 point: video.pts,
                 date: (new Date(video.create)).getTime(),
                 update_date: now_date
@@ -353,7 +329,10 @@ class RankSpider extends Spider {
 }
 
 module.exports = {
-    Spider,
+    CreateRankScheduleJob,
+    CreateVideoScheduleJob,
+    RankData,
+    VideoData,
     VideoSpider,
     RankSpider
 }
