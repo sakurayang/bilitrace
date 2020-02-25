8f64857ec1ee31f710dae0721bb5cfcd46e2bf77
sakurayang <1304274443@qq.com>
2020-02-26 06:28:59 +0800

    fix init bug

--- index.js
+++ index.js
@@ -8,22 +8,21 @@ config.web.enable ?
     server.listen(config.web.port, () => console.log("web start at " + config.web.port)) :
     console.log("web not enable");
 
-if (config.live_enable) {
-    let data = control.File2Json("list.json");
-    if (list in data)
-        for (const live_id of data.list) {
-            if (!live_id.enable) continue;
-            console.log(`Room ${live_id.id} add`);
-            live.add(live_id.id);
-        }
-}
-
-if (config.video_enable) {
-    let data = control.File2Json("cideo.json")
-    if (list in data)
-        for (const video_id of data.list) {
-            if (!video_id.enable) continue;
-            console.log(`Video ${video_id.aid} add`);
-            video.add(video_id.aid, video_id.time);
-        }
-}
+(async () => {
+    if (config.live_enable) {
+        let data = await control.File2Json("live.json");
+        if ("list" in data)
+            for (const live_id of data.list) {
+                if (!live_id.enable) continue;
+                live.add(live_id.id);
+            }
+    }
+    if (config.video_enable) {
+        let data = await control.File2Json("video.json")
+        if ("list" in data)
+            for (const video_id of data.list) {
+                if (!video_id.enable) continue;
+                video.add(video_id.aid, video_id.time);
+            }
+    }
+})();
