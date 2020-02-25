b8e068b4adb853e1855e84f48c60a2583379d901
sakurayang <1304274443@qq.com>
2020-02-26 06:30:19 +0800

    change request mode to querystring

--- server.js
+++ server.js
@@ -13,94 +13,176 @@ const accept = ctx => {
         "Content-Type": "application/json"
     });
 };
-const refuse = ctx => ctx.status = 403;;
+const refuse = ctx => ctx.status = 403;
 
-app.use(_.get('/', ctx => ctx.body = "please visit api diretly"));
+app.use(_.get('/', ctx => refuse(ctx)));
+app.use(_.get('/video', ctx => {
+    accept(ctx);
+    ctx.body = {
+        code: 0,
+        msg: "",
+        list: [
+            "/get",
+            "/add",
+            "/remove",
+            "/update"
+        ]
+    }
+}));
+app.use(_.get('/live', ctx => {
+    accept(ctx);
+    ctx.body = {
+        code: 0,
+        msg: "",
+        list: [
+            "/get",
+            "/add",
+            "/remove",
+            "/living"
+        ]
+    }
+}));
 
 // video route
-app.use(_.get('/video/:id', async (ctx, id) => {
-    console.log("decate:get")
+app.use(_.get('/video/get', async ctx => {
     accept(ctx);
-    id = Number(id);
-    let result = await video.read(id, false);
-    ctx.body = result;
-}));
-app.use(_.get('/video/:id/init', async (ctx, id) => {
-    console.log("decate:init")
-    accept(ctx);
-    id = Number(id);
-    let result = await video.read(id, true);
-    ctx.body = result;
-}));
-
-app.use(_.get('/video/:id/add', async (ctx, id) => {
-    console.log("decate:add1")
-    accept(ctx);
-    id = Number(id);
     let query = ctx.query;
-    let time = "time" in query ?
-        String(query.time).endsWith(" ") ?
-        String(query.time).substring(0, String(query.time).length - 1) :
-        String(query.time) :
-        "*/5 * * * *";
-    let result = await video.add(id, time);
-    ctx.body = result;
+    if ("id" in query) {
+        let id = Number(query.id);
+        let init = "init" in query ? query.init : false;
+        let result = await video.read(id, init);
+        ctx.body = result;
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
 }));
 
-app.use(_.get('/video/:id/remove', async (ctx, id) => {
-    console.log("decate:remove")
+app.use(_.get('/video/add', async ctx => {
     accept(ctx);
-    id = Number(id);
-    let result = await video.remove(id);
-    ctx.body = result;
+    let query = ctx.query;
+    if ("id" in query) {
+        let id = Number(query.id);
+        let time = "time" in query ? (
+            String(query.time).endsWith(" ") ?
+            String(query.time).substring(0, String(query.time).length - 1) :
+            String(query.time)
+        ) : "*/5 * * * *";
+        let result = await video.add(id, time);
+        ctx.body = result;
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
 }));
 
-app.use(_.get('/video/:id/update/:time', async (ctx, id, time) => {
-    console.log("decate:update")
+app.use(_.get('/video/remove', async ctx => {
     accept(ctx);
-    id = Number(id);
-    time = String(time).endsWith(" ") ?
-        String(time).substring(0, String(time).length - 1) :
-        String(time);
-    let result = await video.update(id, time);
-    ctx.body = result;
+    let query = ctx.query;
+    if ("id" in query) {
+        let id = Number(query.id);
+        let result = await video.remove(id);
+        ctx.body = result;
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
+}));
+
+app.use(_.get('/video/update', async ctx => {
+    accept(ctx);
+    let query = ctx.query;
+    if ("id" in query) {
+        let id = Number(query.id);
+        let time = "time" in query ?
+            String(query.time).endsWith(" ") ?
+            String(query.time).substring(0, String(query.time).length - 1) :
+            String(query.time) :
+            "*/5 * * * *";
+        let result = await video.update(id, time);
+        ctx.body = result;
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
 }));
 // ============ end Video route ===========
 
 // live route
-app.use(_.get('/live/:id', async (ctx, id) => {
-    console.log("decate:live get")
+app.use(_.get('/live/get', async ctx => {
     accept(ctx);
-    ctx.body = await live.read(id, 0);
+    let query = ctx.query;
+    if ("id" in query) {
+        let id = Number(query.id);
+        let init = "init" in query ? query.init : false;
+        let result = await live.read(id, init);
+        ctx.body = result;
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
 }));
 
-app.use(_.get('/live/:id/init', async (ctx, id) => {
-    console.log("decate:live init ")
-    accept(ctx);
-    ctx.body = await live.read(id, 1);
-}));
-
-app.use(_.get('/live/:id/living', async (ctx, id) => {
-    console.log("decate:live status")
+app.use(_.get('/live/living', async ctx => {
     ctx.status = 200;
     ctx.set({
         "Content-Type": "text/plain"
     });
-    let info = await request.get(`https://api.live.bilibili.com/room/v1/Room/room_init?id=${id}`);
-    info = JSON.parse(info);
-    ctx.body = info.data.live_status;
+    let query = ctx.query;
+    if ("id" in query) {
+        let id = Number(query.id);
+        let info = await request.get(`https://api.live.bilibili.com/room/v1/Room/room_init?id=${id}`);
+        info = JSON.parse(info);
+        ctx.body = info.data.live_status;
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
 }));
 
-app.use(_.get('/live/:id/add/', async (ctx, id) => {
-    console.log("decate:live add")
+app.use(_.get('/live/add/', async ctx => {
     accept(ctx);
-    ctx.body = await live.add(id);
+    let query = ctx.query;
+    if ("id" in query) {
+        let id = Number(query.id);
+        let result = await live.add(id);
+        ctx.body = result;
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
 }));
 
-app.use(_.get('/live/:id/remove', async (ctx, id) => {
-    console.log("decate:live remove")
+app.use(_.get('/live/remove', ctx => {
     accept(ctx);
-    ctx.body = await live.cancel(id);
+    let query = ctx.query;
+    if ("id" in query) {
+        let id = Number(query.id);
+        live.cancel(id);
+        ctx.body = {
+            code: 0,
+            msg: ""
+        };
+        return;
+    }
+    ctx.body = {
+        code: -1,
+        msg: "id error"
+    }
 }));
 
 // ============end live route=============
