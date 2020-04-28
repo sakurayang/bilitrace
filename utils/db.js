1ab1b2e7b65f04a541067e52ab25eb0af5fc86cb
sakurayang <1304274443@qq.com>
2020-04-28 19:31:37 +0800

    change to knex

--- utils/db.js
+++ utils/db.js
@@ -1,187 +1,192 @@
-//[x]TODO 适配video与rank
 const config = require("../config");
 const path = require("path");
 const g_data_path = config.data_path;
+const debug = config.debug;
+const getKnexOptions = filename => {
+	return {
+		client: "sqlite3",
+		useNullAsDefault: true,
+		connection: async () => ({
+			filename: path.join(g_data_path, filename),
+		}),
+		debug,
+	};
+};
 
 function parseObject(obj) {
-    let keys = Object.keys(obj);
-    let values = Object.values(obj);
-    let transfrom = [];
-    let isArray = Array.isArray(obj);
+	let keys = Object.keys(obj);
+	let values = Object.values(obj);
+	let transfrom = [];
+	let isArray = Array.isArray(obj);
 
-    for (const key in obj) {
-        isArray ? transfrom[key] = obj[key] : transfrom.push(`${key}=${obj[key]}`);
-    }
-    return {
-        keys,
-        values,
-        length: keys.length,
-        transfrom,
-        string: transfrom.toString().replace(",", " ")
-    };
+	for (const key in obj) {
+		isArray
+			? (transfrom[key] = obj[key])
+			: transfrom.push(`${key}=${obj[key]}`);
+	}
+	return {
+		keys,
+		values,
+		length: keys.length,
+		transfrom,
+		sql_string: transfrom.toString().replace(",", " and "),
+	};
 }
 
 /**
- * 
- * @param {String} filename
- * @param {String} table 
- * @param {String} params
- * @return {JSON}
- */
-async function select(filename, table, params = []) {
-    let DB = require('better-sqlite3')(path.join(g_data_path, filename));
-    try {
-        let last_num = await getCount(filename, table, params);
-        let result = await DB.prepare(`SELECT * FROM "${table}"` +
-            `${Object.keys(params).length == 0 ? ' ' : "WHERE " + parseObject(params).string}` + " " +
-            ` LIMIT 1 OFFSET ${last_num - 1}`).get();
-        return {
-            code: 0,
-            msg: "",
-            result
-        };
-    } catch (error) {
-        return {
-            code: -1,
-            msg: error
-        };
-    }
-}
-async function getCount(filename, table, params = []) {
-    let DB = require('better-sqlite3')(path.join(g_data_path, filename));
-    try {
-        let count = await DB.prepare(`SELECT count(*) FROM "${table}"` +
-            `${Object.keys(params).length == 0 ? ' ' : "WHERE " + parseObject(params).string}`).get()['count(*)'];
-        return count;
-    } catch (error) {
-        return {
-            code: -1,
-            msg: error
-        };
-    }
-}
-/**
- * 
- * @param {String} filename
- * @param {String} table 
- * @param {Number} limit
- * @param {Number} offset
- * @returns {{count: Number, 
-    result: Array<JSON>}}
- */
-async function selectAll(filename, table, params = [], limit = 0, offset = 0) {
-    let DB = require('better-sqlite3')(path.join(g_data_path, filename));
-    try {
-        let result = [];
-        let count = await getCount(filename, table);
-        limit = limit !== 0 ? limit : count;
-        //console.log(count);
-        //console.log(limit);
-        for (let i = 0; i < Math.abs(limit - offset); i++) {
-            if (i > count - offset - 1) break;
-            //console.log(i, parseObject(where).string);
-            let db_data = await DB.prepare(`SELECT * FROM "${table}"` +
-                `${Object.keys(params).length == 0 ? ' ' : "WHERE " + parseObject(params).string} ` + " " +
-                ` LIMIT 1 OFFSET ${offset + i}`).get();
-            result.push(db_data);
-        }
-        return {
-            code: 0,
-            msg: "",
-            count,
-            result
-        };
-    } catch (error) {
-        return {
-            code: -1,
-            msg: error
-        };
-    }
-}
-/**
- * 
- * @param {String} filename
- * @param {String} table 
- * @param {JSON} values
- */
-async function insert(filename, table, values) {
-    let DB = require('better-sqlite3')(path.join(g_data_path, filename));
-    //console.log(parseObject(values).keys.toString())
-    //console.log(parseObject(values).values.toString())
-    //console.log(values);
-    try {
-        await DB.prepare(`INSERT INTO "${table}" (${parseObject(values).keys.toString()}) VALUES (${parseObject(values).values.toString()})`).run();
-        return {
-            code: 0,
-            msg: ""
-        }
-    } catch (error) {
-        console.log(error);
-        return {
-            code: -1,
-            msg: error
-        }
-    }
-
-}
-
-/**
- * 
- * @param {String} filename
- * @param {String} table 
- * @param {{
-    time: Date,
-    views: Number,
-    gift: Number,
-    silver: Number,
-    gold: Number
-    }} params
- */
-function delect(filename, table, params) {
-    let DB = require('better-sqlite3')(path.join(g_data_path, filename));
-    try {
-        DB.prepare(`DELETE FROM "${table}" WHERE ${parseObject(params).string}`).run();
-        return {
-            code: 0,
-            msg: ""
-        }
-    } catch (error) {
-        return {
-            code: -1,
-            msg: error
-        }
-    }
-
-}
-
-/**
- * 
+ *
  * @param {String} filename
  * @param {String} table
- * @param {JSON} params where
+ * @param {JSON[]} params
+ * @return {JSON}
+ */
+async function select(filename, table, params = [], limit = 1, offset = -1) {
+	if (debug)
+		console.log(
+			`[select] [${filename} - ${table}] params: ${JSON.stringify(
+				params
+			)}, limit: ${limit}, offset: ${offset}`
+		);
+	const knex = require("knex")(getKnexOptions(filename));
+	try {
+		let last_num = (await getCount(filename, table, params)).result;
+		let result = await knex(table)
+			.where(params)
+			.select("*")
+			.limit(limit)
+			.offset(offset === -1 ? last_num - 1 : offset);
+		return {
+			code: 0,
+			msg: "",
+			result,
+		};
+	} catch (error) {
+		console.error(`[select] ${error}`);
+		return {
+			code: -1,
+			msg: error,
+		};
+	}
+}
+
+/**
+ *
+ * @param {String} filename
+ * @param {String} table
+ * @param {JSON[]} params
+ * @return {JSON}
+ */
+async function getCount(filename, table, params = []) {
+	const knex = require("knex")(getKnexOptions(filename));
+	try {
+		let count = await (await knex(table).where(params).select("*")).length;
+		return {
+			code: 0,
+			result: count,
+		};
+	} catch (error) {
+		console.error(`[getCount] ${error}`);
+		return {
+			code: -1,
+			msg: error,
+		};
+	}
+}
+
+/**
+ *
+ * @param {String} filename
+ * @param {String} table
+ * @param {JSON[]} values
+ */
+async function insert(filename, table, values) {
+	if (debug)
+		console.log(
+			`[insert] [${filename} - ${table}] values: ${JSON.stringify(
+				values
+			)}`
+		);
+	const knex = require("knex")(getKnexOptions(filename));
+	try {
+		await knex(table).insert(values);
+		return {
+			code: 0,
+			msg: "",
+		};
+	} catch (error) {
+		console.error(`[insert] ${error}`);
+		return {
+			code: -1,
+			msg: error,
+		};
+	}
+}
+
+/**
+ *
+ * @param {String} filename
+ * @param {String} table
+ * @param {JSON} params
+ */
+// add "_" before function name cause delete is a kept word in js
+function _delete(filename, table, params) {
+	if (debug)
+		console.log(
+			`[delete] [${filename} - ${table}] params: ${JSON.stringify(
+				params
+			)}`
+		);
+	const knex = require("knex")(getKnexOptions(filename));
+	try {
+		knex(table).where(parseObject(params).sql_string).delete();
+		return {
+			code: 0,
+			msg: "",
+		};
+	} catch (error) {
+		console.error(`[delect] ${error}`);
+		return {
+			code: -1,
+			msg: error,
+		};
+	}
+}
+
+/**
+ *
+ * @param {String} filename
+ * @param {String} table
+ * @param {JSON[]|JSON|[]} params where
  * @param {JSON} values new value
  */
 function update(filename, table, params, values) {
-    let DB = require('better-sqlite3')(path.join(g_data_path, filename));
-    try {
-        DB.prepare(`UPDATE ${table} SET '${parseObject(values).string}' WHERE ${parseObject(params).string}'`).run();
-        return {
-            code: 0,
-            msg: ""
-        }
-    } catch (error) {
-        return {
-            code: -1,
-            msg: error
-        }
-    }
+	if (debug)
+		logger.info(
+			`[update] [${filename} - ${table}] from: ${JSON.stringify(
+				params
+			)}, to: ${JSON.stringify(values)}`
+		);
+	const knex = require("knex")(getKnexOptions(filename));
+	try {
+		knex(table).where(parseObject(params).sql_string).update(values);
+		return {
+			code: 0,
+			msg: "",
+		};
+	} catch (error) {
+		logger.error(`[update] ${error}`);
+		return {
+			code: -1,
+			msg: error,
+		};
+	}
 }
 
 module.exports = {
-    select,
-    insert,
-    delect,
-    update,
-    selectAll,
-    getCount
-}
+	getCore: filename => require("knex")(getKnexOptions(filename)),
+	select,
+	insert,
+	delete: _delete,
+	update,
+	getCount,
+};
